edeb014e260441ae063747dccc9d63d572109680
june-js-moon <jeru8603@gmail.com>
2021-01-03 22:58:29 +0900

    cormmit

---
+++ solved.ac.class/CLASS_01/CLASS_01/2_1001.cpp
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int num1, num2;
+
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d", num1 - num2);
+
+	return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
